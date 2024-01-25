1d9149085bcdb314d489ce5b810e153e41307097
TEJMaster <terry20021026@gmail.com>
2024-01-25 05:29:39 +0000

    Fixed the project based on comments

---
+++ scripts/02-cleanup-data.R
@@ -0,0 +1,14 @@
+# Load necessary libraries
+library(readr)
+library(dplyr)
+
+# Read the raw dataset
+subway_delay_data <- read_csv("inputs/data/subway_delay_data_2023.csv")
+
+# Clean the data by removing records with 'NA' in the 'Line' column and data point not in the 4 subway lines in Toronto
+clean_subway_delay_data <- subway_delay_data %>%
+  filter(!is.na(Line))  %>%
+  filter(Line %in% c("BD", "YU", "SHP", "SRT"))
+
+# Save the cleaned data to a new CSV file
+write_csv(x = clean_subway_delay_data, file = "inputs/data/cleaned_subway_delay_data_2023.csv")
---
+++ scripts/03-test-data-validity.R
@@ -0,0 +1,23 @@
+library(readr)
+library(dplyr)
+library(lubridate)
+
+# Load the cleaned subway delay data
+file_path <- "inputs/data/cleaned_subway_delay_data_2023.csv"
+subway_delay_data <- read_csv(file_path)
+
+# Test 1: All Date columns are from year 2023
+test_year_2023 <- all(year(ymd(subway_delay_data$Date)) == 2023)
+
+# Test 2: Min Delay column is >= 0
+test_min_delay_non_negative <- all(subway_delay_data$Min_Delay >= 0)
+
+# Test 3: Line column only includes specific data points
+allowed_lines <- c("BD", "YU", "SHP", "SRT")
+test_line_inclusion <- all(subway_delay_data$Line %in% allowed_lines)
+
+list(
+  test_year_2023 = test_year_2023,
+  test_min_delay_non_negative = test_min_delay_non_negative,
+  test_line_inclusion = test_line_inclusion
+)
